82ae5b4e87e284c0d9465b21cdcf6e65a85a5598
tamontealegrep <montealegre.tomas@outlook.com>
2024-12-01 22:05:51 -0500

    Add importations

---
+++ src/models/architectures/blocks/__init__.py
@@ -0,0 +1,8 @@
+
+from .conv_block import ConvBlock
+from .rec_conv_block import RecConvBlock
+from .res_conv_block import ResConvBlock
+from .rr_conv_block import RRConvBlock
+from .vit_block import ViTBlock
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
